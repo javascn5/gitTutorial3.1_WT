4df4928d1f503bcd66e6fa6153d914b32b1c1fec
werkaska3 <weronika3@o2.pl>
2018-10-20 15:01:57 +0200

    dodalam cos

---
+++ src/User.java
@@ -0,0 +1,15 @@
+public class User {
+    private String name;
+            private String lastname;
+            private int age;
+
+            public User(String name, String lastname, int age) {
+                this.name = name;
+                this.lastname = lastname;
+                        this.age = age;
+age = 18;
+                System.out.println(age);
+            }
+
+
+}
